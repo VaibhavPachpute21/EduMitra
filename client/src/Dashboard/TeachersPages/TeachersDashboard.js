c77324467e133cc7ae6667abce5988b80866ab4c
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-10-22 15:42:41 +0530

    Dashobard Created

--- client/src/Dashboard/TeachersPages/TeachersDashboard.js
+++ client/src/Dashboard/TeachersPages/TeachersDashboard.js
@@ -51,12 +51,11 @@ const TeachersDashboard = () => {
                 </div>
             </div>}
 
-            <div className="container py-4 py-xl-5">
-                <h2>Hello {currentUser.user.name}!!</h2>
-
+            <div className="container py-1 py-xl-5">
+                <img src='assets/img/Dashboard.png' className='m-auto w-100 fit-cover' alt='Dashboard' width={'100%'} height={'100%'}/>
             </div>
 
-            <div className="container py-4 py-xl-5">
+            <div className="container py-xl-5">
                 <div className="row mb-5" style={{ background: 'var(--bs-body-bg)', padding: '12px', boxShadow: '0px 4px 6px 1px rgba(43,49,54,0.35)', borderRadius: '5px' }}>
                     <div className="col-sm-10 col-md-7 col-lg-8">
                         <h2>Projects by Students of {currentUser.user.college}</h2>
@@ -81,35 +80,6 @@ const TeachersDashboard = () => {
                         </div>
                     })}
 
-                </div>
-            </div>
-            <div className="container py-4 py-xl-5">
-                <div className="row mb-5" style={{ background: 'var(--bs-body-bg)', padding: '12px', boxShadow: '0px 4px 6px 1px rgba(43,49,54,0.35)', borderRadius: '5px' }}>
-                    <div className="col-sm-10 col-md-7 col-lg-8">
-                        <h2>Trending Projects</h2>
-                    </div>
-                    <div className="col" style={{ textAlign: 'right' }}>
-                        <Link className="btn btn-primary fs-5 me-2 py-2 px-4" to="/Projects" style={{ borderRadius: '0px' }}>
-                            Explore more
-                        </Link>
-                    </div>
-                </div>
-                <div className="row gy-4 row-cols-1 row-cols-md-2">
-                    {/* Repeat the following card section for each trending project */}
-                    <div className="col">
-                        <a className="text-decoration-none text-black" href="SingleProject.html">
-                            <div className="d-flex flex-column flex-lg-row" data-bss-hover-animate="pulse" style={{ background: 'white', borderRadius: '10px', boxShadow: '2px 4px 7px 1px rgba(43,49,54,0.35)' }}>
-                                <div className="w-100">
-                                    <img className="rounded img-fluid d-block w-100 fit-cover" style={{ height: '200px' }} src="https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/255/061/datas/gallery.jpg" alt="Trending Project" />
-                                </div>
-                                <div className="py-4 py-lg-0 px-lg-4">
-                                    <h4>empath.ly</h4>
-                                    <p>empath.ly uses machine learning to analyze emotions during video calls or metaverse activities. We empower employers, marketers, developers, and the visually impaired to empathize remotely.</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    {/* Repeat this card section for each trending project */}
                 </div>
             </div>
         </section>
